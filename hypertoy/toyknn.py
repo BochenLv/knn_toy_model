d37f054e60c6e31676dba9cd46d524b0ae743139
bochenL <bochen.lv@gmail.com>
2021-06-09 15:28:16 +0800

    new folder

--- hypertoy/toyknn.py
+++ hypertoy/toyknn.py
@@ -2,7 +2,6 @@
 
 """
 import pickle
-import hypertoy
 
 from sklearn import pipeline as sk_pipeline
 from hypertoy.pipeline import ComposeTransformer
--- sklearn/sklearn_ops.py
+++ sklearn/sklearn_ops.py
@@ -7,10 +7,10 @@ preprocessing and assembling them into the full-pipeline, which appear in the se
 before the data being sent to estimators.
 """
 import numpy as np
-from .search_space import sear_space_eg
+from hypertoy.search_space import sear_space_eg
 
-from cfg import KnnCfg as cfg
-from pipeline import Pipeline
+from hypertoy.cfg import KnnCfg as cfg
+from hypertoy.pipeline import Pipeline
 from sklearn.transformers import SimpleImputer, SafeOneHotEncoder, TruncatedSVD, \
     StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, SafeOrdinalEncoder, \
     LogStandardScaler, DatetimeEncoder, TfidfEncoder
